85724faac8fe91d7ba5e931925ba8ebdcc2221d2
bsitenga <sitenga@usc.edu>
2020-08-02 23:31:18 -0400

    added nightmare action

--- app.js
+++ app.js
@@ -4,5 +4,18 @@ const Nightmare = require('nightmare');
 const nightmare = Nightmare({ show: true });
 
 const getUsers = () => {
+	console.log('tester');
+
+	Nightmare.action('scrollIntoView', function(selector, done) {
+		this.evaluate_now(
+			(selector) => {
+				// https://developer.mozilla.org/en-US/docs/Web/API/Element/scrollIntoView
+				document.querySelector(selector).scrollIntoView(true);
+			},
+			done,
+			selector
+		);
+    });
     
-}
+    
+};
