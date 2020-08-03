966ee8721dca5301b1f40c5f8bd68f99a381b136
bsitenga <sitenga@usc.edu>
2020-08-02 23:43:53 -0400

    fixed export error

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ import cheerio from 'cheerio';
 import Nightmare from 'nightmare';
 const nightmare = Nightmare({ show: true });
 
-export const getUsers = () => {
+const getUsers = () => {
 	console.log('tester');
     
 	Nightmare.action('scrollIntoView', function(selector, done) {
@@ -16,5 +16,23 @@ export const getUsers = () => {
 		);
     });
     
+    nightmare
+    .goto('https://www.instagram.com/')
+    .wait('input[name="username"]')
+    .type('input[name="username"]', 'spicycarbonara')
+    .type('input[name="password"]', 'BrianSitenga99')
+    .click('button[type="submit"]')
+    .wait('input[placeholder="Search"]')
+    .type('input[placeholder="Search"]', 'yayforearth')
+    .click('input[placeholder="Search"]')
+    .wait('div.fuqBx a')
+    .click('div.fuqBx a')
+    .wait('h2._7UhW9')
+    .click('div.v1Nh3 a')
+    .wait('button.sqdOP span')
+    .click('button.sqdOP span')
+    .wait('a.notranslate')
+    .scrollIntoView('a.notranslate:last-child')
+    .wait('div[style="height: 356px; overflow-y: auto; overflow-x: hidden;"]')
 
 };
