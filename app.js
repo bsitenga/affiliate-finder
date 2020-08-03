68406ea9bc23ade1b6ba73527d31a8fc5f9d6382
bsitenga <sitenga@usc.edu>
2020-08-02 23:17:47 -0400

    test for github pages

--- app.js
+++ app.js
@@ -1,3 +1,3 @@
-const app = document.getElementById("app");
-window.run = () => app.innerText="Fun!";
-app.innerHTML = '<button onclick="run()">Load</button>';
+const getUsers = () => {
+    console.log("hello");
+}
